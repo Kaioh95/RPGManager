893f92f3831eafce9d99f58e479544d96c3e5749
João Victor Fernandes Cabral <jgazbral@gmail.com>
2022-02-04 20:49:33 -0300

    setup para busca e preenchimento dos campos na API

--- app/src/main/java/com/imd/rpgmanager/MonstrosActivity.java
+++ app/src/main/java/com/imd/rpgmanager/MonstrosActivity.java
@@ -3,15 +3,31 @@ package com.imd.rpgmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class MonstrosActivity extends AppCompatActivity {
+
+    EditText edMonster;
+    Button searchMonster;
+    TextView txtMonsterName;
+    TextView txtMonsterType;
+    TextView txtMonsterLvl;
+    TextView txtMonsterHealth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monstros);
 
+        edMonster = findViewById(R.id.edMonsterName);
+        searchMonster = findViewById(R.id.searchMonsterBtn);
+        txtMonsterName = findViewById(R.id.monsterNameGET);
+        txtMonsterType = findViewById(R.id.monsterTypeGET);
+        txtMonsterLvl = findViewById(R.id.monsterLvlGET);
+        txtMonsterHealth = findViewById(R.id.monsterHealthGET);
     }
 }
